ca66b9fe3f481d21224cb1fba5f8ec58f75018ad
danilppzz <josedanielmorebolopez@gmail.com>
2024-09-25 23:02:48 +0200

    Update Counter.tsx

--- src/components/ui/Counter.tsx
+++ src/components/ui/Counter.tsx
@@ -5,7 +5,8 @@ const Counter: React.FC = () => {
     const [count, setCount] = useState(0);
 
     const increment = () => setCount(count + 1);
-    const decrease = () => setCount(count > 0 ? count - 1 : count);
+    const decrease = () => setCount(count - 1);
+    const reset = () => setCount(0);
 
     return (
         <div>
@@ -13,6 +14,7 @@ const Counter: React.FC = () => {
                 <button className='w-8 h-8 p-auto text-white/40 rounded-lg bg-c-300' onClick={decrease}>-</button>
                 <button className='w-14 h-8 p-auto text-white/40 rounded-lg bg-c-300' onClick={increment}>{count}</button>
                 <button className='w-8 h-8 p-auto text-white/40 rounded-lg bg-c-300' onClick={increment}>+</button>
+                <button className='min-w-8 h-8 p-auto px-3 text-white/40 rounded-lg bg-c-300 hover:bg-c-400/65 transition-all ease-in-out duration-200' onClick={reset}>reset</button>
             </div>
         </div>
     );
